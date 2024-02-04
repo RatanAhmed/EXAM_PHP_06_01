edcb1ad658638f71e4b5e3157b280465855718eb
Ratan <ratanahmed690@gmail.com>
2024-02-04 22:06:23 +0600

    Datatable

---
+++ app/Http/Controllers/MyColleagueController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MyColleague;
+use Illuminate\Http\Request;
+
+class MyColleagueController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(MyColleague $myColleague)
+    {
+        //
+    }
+
+    public function edit(MyColleague $myColleague)
+    {
+        //
+    }
+
+    public function update(Request $request, MyColleague $myColleague)
+    {
+        //
+    }
+
+    public function destroy(MyColleague $myColleague)
+    {
+        //
+    }
+}
